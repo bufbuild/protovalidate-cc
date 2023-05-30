d002a84a5ff592fa0e64d7427a9f437d972e9aca
Alan Chiu <awlchiu@gmail.com>
2023-05-30 10:20:06 -0700

    Add contains function

--- buf/validate/internal/extra_func.cc
+++ buf/validate/internal/extra_func.cc
@@ -8,6 +8,17 @@
 namespace buf::validate::internal {
 namespace cel = google::api::expr::runtime;
 
+cel::CelValue contains(
+    google::protobuf::Arena* arena, cel::CelValue::BytesHolder lhs, cel::CelValue rhs) {
+  if (!rhs.IsBytes()) {
+    auto* error = google::protobuf::Arena::Create<cel::CelError>(
+        arena, absl::StatusCode::kInvalidArgument, "doesnt start with the right thing");
+    return cel::CelValue::CreateError(error);
+  }
+  bool result = absl::StrContains(lhs.value().data(), rhs.BytesOrDie().value());
+  return cel::CelValue::CreateBool(result);
+}
+
 cel::CelValue startsWith(
     google::protobuf::Arena* arena, cel::CelValue::BytesHolder lhs, cel::CelValue rhs) {
   if (!rhs.IsBytes()) {
@@ -45,6 +56,12 @@ absl::Status RegisterExtraFuncs(
           &registry);
   if (!status.ok()) {
     return status;
+  }
+  auto containsStatus =
+      cel::FunctionAdapter<cel::CelValue, cel::CelValue::BytesHolder, cel::CelValue>::
+          CreateAndRegister("contains", true, &contains, &registry);
+  if (!containsStatus.ok()) {
+    return containsStatus;
   }
   auto startsWithStatus =
       cel::FunctionAdapter<cel::CelValue, cel::CelValue::BytesHolder, cel::CelValue>::
