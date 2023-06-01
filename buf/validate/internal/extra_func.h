86950156e988fb24a5925a011b42c25f9917e59b
Elliot Jackson <13633636+elliotmjackson@users.noreply.github.com>
2023-06-01 12:25:57 -0400

    Remove unnecessary file (#10)

--- buf/validate/internal/extra_func.h
+++ buf/validate/internal/extra_func.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <arpa/inet.h>
+
+#include <functional>
+#include <stdexcept>
+#include <string>
+#include <typeindex>
+#include <typeinfo>
+#include <unordered_map>
+
 #include "absl/status/status.h"
 #include "eval/public/cel_function_registry.h"
+#include "google/protobuf/message.h"
+#include "re2/re2.h"
 
 namespace buf::validate::internal {
 
 absl::Status RegisterExtraFuncs(
     google::api::expr::runtime::CelFunctionRegistry& registry, google::protobuf::Arena* regArena);
 
-}
+} // namespace buf::validate::internal
