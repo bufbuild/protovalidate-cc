5c7707fd2086210cf84a2253f6ab7f3c28c9e40c
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2023-06-01 10:53:05 -0400

    checkpoint

--- buf/validate/internal/extra_func.cc
+++ buf/validate/internal/extra_func.cc
@@ -12,6 +12,7 @@
 
 namespace buf::validate::internal {
 namespace cel = google::api::expr::runtime;
+using std::string;
 
 cel::CelValue unique(google::protobuf::Arena* arena, cel::CelValue rhs) {
   if (!rhs.IsList()) {
@@ -64,6 +65,28 @@ cel::CelValue endsWith(
   return cel::CelValue::CreateBool(result);
 }
 
+bool IsHostname(const string& to_validate) {
+  if (to_validate.length() > 253) {
+    return false;
+  }
+  const re2::RE2 component_regex("^[A-Za-z0-9]+(?:-[A-Za-z0-9]+)*$");
+  std::vector<std::string> split = absl::StrSplit(to_validate, '.');
+  std::vector<std::string> search = {split.begin(), split.end() - 1};
+  if (split.size() < 2) {
+    return re2::RE2::FullMatch(split[0], component_regex);
+  }
+  std::string last = split[split.size() - 1];
+  for (const std::string& part : search) {
+    if (part.empty() || part.size() > 63) {
+      return false;
+    }
+    if (!re2::RE2::FullMatch(part, component_regex)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 cel::CelValue isHostname(google::protobuf::Arena* arena, cel::CelValue::StringHolder lhs) {
   std::string s(lhs.value());
   return cel::CelValue::CreateBool(IsHostname(s));
@@ -93,6 +116,20 @@ cel::CelValue isEmail(google::protobuf::Arena* arena, cel::CelValue::StringHolde
   // Validate the hostname
   std::string s(domainPart);
   return cel::CelValue::CreateBool(IsHostname(s));
+}
+
+bool IsIpv4(const string& to_validate) {
+  struct sockaddr_in sa;
+  return !(inet_pton(AF_INET, to_validate.c_str(), &sa.sin_addr) < 1);
+}
+
+bool IsIpv6(const string& to_validate) {
+  struct sockaddr_in6 sa_six;
+  return !(inet_pton(AF_INET6, to_validate.c_str(), &sa_six.sin6_addr) < 1);
+}
+
+bool IsIp(const string& to_validate) {
+  return IsIpv4(to_validate) || IsIpv6(to_validate);
 }
 
 cel::CelValue isIPvX(
